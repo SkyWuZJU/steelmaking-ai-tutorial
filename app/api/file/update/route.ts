f1aa73aceb1a9a79e0d090befd4d0d342d701a9e
Sky <sky_wu@zju.edu.cn>
2024-11-16 13:49:04 +0800

    - fix the issue in retriving all files - Use context to manage the statement of knowledge page (ongoing) - components on knowledge page (ongoing) - complete the api/file/remove - complete the knowledge retrieval and injection in steelmaking expert agent

---
+++ app/api/file/update/route.ts
@@ -0,0 +1,6 @@
+import { KnowledgeFile } from "@/lib/types";
+import { generateId } from "ai";
+
+export async function POST(request: Request) {
+    // TODO: Implement file update after the frontend is ready
+}
